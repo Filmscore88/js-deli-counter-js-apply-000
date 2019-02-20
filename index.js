5e4e3a8f4573cc6a1240c348e06d4976af50c122
Alberto Morales <cmtmorales@gmail.com>
2019-02-20 19:23:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ function takeANumber(currentLine,name){
   var greet=("Welcome, "+name+"."+" You are number "+currentLine.length+" in line.");
   return greet;
 }
+
+
+function nowServing(currentLine){
+  var x=currentLine.index[0];
+}
